5a622b189c17546efc16a73f4970b22a21f1da5a
fangcj <1092301692@qq.com>
2021-02-18 17:24:39 +0800

    oneday

---
+++ ClusterBatch/ClusterBatch/src/machine/bean/MachineBean.cpp
@@ -0,0 +1,17 @@
+/*
+ * MachineBean.cpp
+ *
+ *  Created on: 2021Äê2ÔÂ18ÈÕ
+ *      Author: fangchunjing
+ */
+
+#include "MachineBean.h"
+
+MachineBean::MachineBean() {
+
+}
+
+MachineBean::~MachineBean() {
+	// TODO Auto-generated destructor stub
+}
+
