665d52122734e173f110b5d648d2fe451e11b5eb
fangcj <1092301692@qq.com>
2021-02-18 16:27:53 +0800

    初始化提交批量任务

---
+++ ClusterBatch/ClusterBatch/src/util/Machine.cpp
@@ -0,0 +1,18 @@
+/*
+ * Machine.cpp
+ *
+ *  Created on: 2021Äê2ÔÂ18ÈÕ
+ *      Author: fangchunjing
+ */
+
+#include "Machine.h"
+
+Machine::Machine() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Machine::~Machine() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ ClusterBatch/ClusterBatch/src/util/Machine.h
@@ -0,0 +1,17 @@
+/*
+ * Machine.h
+ *
+ *  Created on: 2021Äê2ÔÂ18ÈÕ
+ *      Author: fangchunjing
+ */
+
+#ifndef UTIL_MACHINE_H_
+#define UTIL_MACHINE_H_
+
+class Machine {
+public:
+	Machine();
+	virtual ~Machine();
+};
+
+#endif /* UTIL_MACHINE_H_ */
